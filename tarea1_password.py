b2b02fc315a1f07bd0d0882fcdcff09a7afa1450
MariE <marieug.mc@gmail.com>
2022-11-07 17:08:42 -0400

    cambios2

--- tarea1_password.py
+++ tarea1_password.py
@@ -17,3 +17,44 @@ def contras():
         passw6 = passw6 + passw5  
     codigo = passw6 + passw4
     return codigo
+
+#while True:
+def menus():
+    print("MENU\n1. Generar codigo\n2. obtener codigo\n3. salir")
+    opc = input("Ingrese una opcion: ")
+    return opc
+while True:
+    cuentas = []
+    opcion = menus()
+    while opcion == "1":
+        cuenta = input("ingrese la cuenta para la cual quiere generar codigo: ")
+        x = cuenta
+        y = contras()
+        dic = {
+            'cuen': x,
+            'contras': y
+        }
+        #x = cuenta
+        #y = contras()
+        cuentas.append({'cuent': x, 'contras': y})
+        print(cuentas)
+        #print("la contraseña fue generada con exito, elija otra opcion")
+        resp = input("la contraseña fue generada con exito, quiere generar otra contraseña?:\n 1. si\n 2. No\n")
+        if resp == "2":
+            menus()
+        #print("MENU\n1. Generar codigo\n2. obtener codigo\n3. salir")
+        #opcion = input("Ingrese una opcion: ")
+        opcion2 = menus()
+        while opcion2 == "2":
+            print(cuentas)
+            for c in cuentas:
+                buscar = input("ingrese la cuenta de la cual quiere ver la contraseña: ")
+                #print ("password: ", cuentas.get(buscar, "no entramos la cuenta"))
+                if buscar == c['cuent']:
+                    print('tu password de ' + buscar + ' es: ', c['contras'])
+                    resp2 = input("quiere ver otra contraseña?:\n 1. si\n 2. No\n")
+                if resp2 == "2":
+                    menus()
+                #opcion3 = menu ()
+    if opcion == "3":
+        break
